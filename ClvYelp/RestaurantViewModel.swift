f231b793f45ab4d93b47d2265a52005c01082d95
sonnguyen <sonnguyen@dev1s-MacBook-Pro.local>
2021-06-18 14:46:33 -0400

    fix mvvm bug, View should not see Model, only ViewModel

--- ClvYelp/RestaurantViewModel.swift
+++ ClvYelp/RestaurantViewModel.swift
@@ -11,12 +11,39 @@ import Foundation
 struct RestaurantViewModel {
     
     private let restaurant: YelpRestaurant
-    
+    //private let rest: YelpRst
+
     var displayText: String {
         return restaurant.name?.capitalized ?? "" + " - " + (restaurant.displayphone!)
     }
     
+    var name: String? {
+        return restaurant.name //?? "UnknownName"
+    }
+    
+    var address: [String]? {
+        return restaurant.address
+    }
+    
+    var displayphone: String? {
+        return restaurant.displayphone
+    }
+    
+    var imageurl: String? {
+        return restaurant.imageurl
+    }
+    
+    var review: Int {
+        return restaurant.review
+    }
+    
     init(restaurant: YelpRestaurant) {
         self.restaurant = restaurant
+        //self.rest = YelpRst(restaurantArray: self.restaurant, result: false)
     }
+}
+
+struct YelpRetrievedList {
+    var restaurantArray: [RestaurantViewModel?]
+    var result: Bool
 }
